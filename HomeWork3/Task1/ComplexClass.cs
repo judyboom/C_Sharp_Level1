7c903b1a7be0e839f9145aa26779d8f1a6d586df
judyboom <judybooms@gmail.com>
2018-12-20 22:54:12 +0300

    HW3 is done

---
+++ HomeWork3/Task1/ComplexClass.cs
@@ -0,0 +1,72 @@
+﻿/*
+ HomeWork3 by Yuliya Reshetova 
+ Задача 1:
+б) Дописать класс Complex, добавив методы вычитания и произведения чисел. Проверить работу класса.
+ */
+namespace Task1
+{
+    public class ComplexClass
+    {
+        double im;
+        double re;
+
+        public ComplexClass()
+        {
+            im = 0;
+            re = 0;
+        }
+
+        public ComplexClass(double _im, double re)
+        {
+            im = _im;
+            this.re = re;
+        }
+        public ComplexClass Plus(ComplexClass x2)
+        {
+            ComplexClass x3 = new ComplexClass();
+            x3.im = x2.im + im;
+            x3.re = x2.re + re;
+            return x3;
+        }
+
+        public ComplexClass Minus(ComplexClass x)
+        {
+            ComplexClass y = new ComplexClass();
+            y.im = im - x.im;
+            y.re = re - x.re;
+            return y;
+        }
+
+        public ComplexClass Multi(ComplexClass x)
+        {
+            ComplexClass y = new ComplexClass();
+            y.im = re * x.im + im * x.re;
+            y.re = re * x.re - im * x.im;
+            return y;
+        }
+
+        public double Im
+        {
+            get { return im; }
+            set
+            {
+                if (value >= 0) im = value;
+            }
+        }
+
+        public double Re
+        {
+            get { return re; }
+            set
+            {
+                if (value >= 0) re = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return re + ((im < 0) ? "" : "+") + im + "i";
+        }
+
+    }
+}
---
+++ HomeWork3/Task3/Fraction.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Task3
+{
+    class Fraction
+    {
+        int Num;
+        int Denom;
+
+        public Fraction(int num, int denom)
+        {
+            Num = num;
+            if (denom == 0)
+            {
+                throw new ArgumentException("Знаменатель не может быть равен 0");
+            }
+            else Denom = denom;
+        }
+
+        public Fraction Sum(Fraction fraction)
+        {
+            var num = Num * fraction.Denom + fraction.Num * Denom;
+            var denom = Denom * fraction.Denom;
+            Fraction result = new Fraction(num, denom);
+            return result.Simplify();
+        }
+
+        public Fraction Minus(Fraction fraction)
+        {
+            var num = Num * fraction.Denom - fraction.Num * Denom;
+            var denom = Denom * fraction.Denom;
+            Fraction result = new Fraction(num, denom);
+            return result.Simplify();
+        }
+
+        public Fraction Multy(Fraction fraction)
+        {
+            var num = Num * fraction.Num;
+            var denom = Denom * fraction.Denom;
+            Fraction result = new Fraction(num, denom);
+            return result.Simplify();
+        }
+
+        public Fraction Div(Fraction fraction)
+        {
+            return Multy(fraction.Reverse());
+        }
+
+        public Fraction Reverse()
+        {
+            return new Fraction(Denom, Num);
+        }
+
+        public Fraction Simplify()
+        {
+            int nod = NOD(Num, Denom);
+            Num /= nod;
+            Denom /= nod;
+            return this;
+        }
+
+        static int NOD(int a, int b)
+        {
+            while (a != b)
+                if (a > b) a = a - b; else b = b - a;
+            return a;
+        }
+
+        public override string ToString()
+        {
+            return Num  + ((Denom == 1) ? "": "/" + Denom.ToString());
+        }
+
+    }
+}
